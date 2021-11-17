53360926485184a82ebd7ea93f61df4c30351208
usplm <admin@usplm.net>
2021-11-17 03:42:53 -0500

    Updated instruction

--- app/src/components/Directions.js
+++ app/src/components/Directions.js
@@ -22,7 +22,7 @@ class Directions extends Component {
           <ol>
             <li>
               <span className='tc'><span className='circled'>1</span></span>
-              <span className='tc'>Upload images (jpeg or png).</span></li>
+              <span className='tc'>Choose images.</span></li>
             <li>
               <span className='tc'><span className='circled'>2</span></span>
               <span className='tc'>Set a point in an image.</span>

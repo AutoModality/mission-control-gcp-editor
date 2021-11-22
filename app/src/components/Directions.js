8baec18a491cb1d353e3ff3eacd8d0eddecbf21f
usplm <admin@usplm.net>
2021-11-22 09:21:39 -0500

    Updated instruction steps

--- app/src/components/Directions.js
+++ app/src/components/Directions.js
@@ -18,25 +18,30 @@ class Directions extends Component {
       <div className={`directions ${statusClass}`}>
         <h3 onClick={this.onClick}><span className='arrow'></span>Directions</h3>
         <div className='direction-content'>
-          <p>Connect at least 5 high-contrast objects in 3 or more photos to their corresponding locations on the map.</p>
+          <p>Connect at least 5 high-contrast objects in 3 or more images to their corresponding locations on the map.</p>
           <ol>
-            <li>
+          <li>
               <span className='tc'><span className='circled'>1</span></span>
-              <span className='tc'>Choose images.</span></li>
+              <span className='tc'>Load predefined ground control points. (Optional)</span>
+            </li>
             <li>
               <span className='tc'><span className='circled'>2</span></span>
-              <span className='tc'>Set a point in an image.</span>
+              <span className='tc'>Choose mission images.</span>
             </li>
             <li>
               <span className='tc'><span className='circled'>3</span></span>
-              <span className='tc'>Set a corresponding point on the map.</span>
+              <span className='tc'>Set a point in an image.</span>
             </li>
             <li>
               <span className='tc'><span className='circled'>4</span></span>
-              <span className='tc'>Repeat as desired (at least until the goal is achieved).</span>
+              <span className='tc'>Select or set a corresponding point on the map.</span>
             </li>
             <li>
               <span className='tc'><span className='circled'>5</span></span>
+              <span className='tc'>Repeat step 2-4 until at least 5 objects in 3 or more images are connected.</span>
+            </li>
+            <li>
+              <span className='tc'><span className='circled'>6</span></span>
               <span className='tc'>Save/Export the ground control points.</span>
             </li>
           </ol>
